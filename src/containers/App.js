6f4fd4d7a03d80bc8d0617262b16a50806172a38
Alex Chukhrai <zimmalex6@gmail.com>
2016-11-14 02:24:07 +0200

    add new data

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class App extends Component {
-  render() {
-    return <div>Привет из App</div>
-  }
+class App extends Component{
+    render (){
+        const {name , surname, age } = this.props.user
+        return <div> Привет из App, {this.props.user} !</div>
+    }
 }
+
+function mapStateToProps(state){
+    return {
+        user: state.user
+    }
+}
+
+export default connect (mapStateToProps)(App)
